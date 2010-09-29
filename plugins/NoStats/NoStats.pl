8d37ef05f0f00bcd24b7279e73c53a6afeef723b
Finn Smith <finn@timeghost.net>
2010-09-29 10:45:19 -0700

    Cleanup whitespace.

--- plugins/NoStats/NoStats.pl
+++ plugins/NoStats/NoStats.pl
@@ -1,4 +1,3 @@
-
 package MT::Plugin::NoStats;
 
 use strict;
@@ -12,69 +11,68 @@ $VERSION = '0.31';
 $PLUGIN_ID = 'no_stats';
 
 my $plugin = MT::Plugin::NoStats->new ({
-	id => $PLUGIN_ID,
+    id => $PLUGIN_ID,
     name        => 'NoStats',
     description => 'No stats!',
     version     => $VERSION,
 
     author_name => 'Apperceptive, LLC',
     author_link => 'http://www.apperceptive.com/',
-    
 });
 MT->add_plugin ($plugin);
 
 sub init_registry {
-	my $component = shift;
-	my $reg = {
+    my $component = shift;
+    my $reg = {
 
-		author 		=> 'Apperceptive, LLC',
-    	init_app            => \&initialize,
-    	callbacks => {
-			'MT::App::CMS::template_source.blog_stats'   => \&source_dashboard,
-    	},
-	};
-	$component->registry($reg);
+        author      => 'Apperceptive, LLC',
+        init_app            => \&initialize,
+        callbacks => {
+            'MT::App::CMS::template_source.blog_stats'   => \&source_dashboard,
+        },
+    };
+    $component->registry($reg);
 }
 
 sub initialize {
-	my $plugin = shift;
-	my ($app) = @_;
-	eval { require MT::Community::CMS; };
-	if (!$@) {
-		my $mt_most_popular_entries_widget = \&MT::Community::CMS::most_popular_entries_widget;
-		{
-			local $SIG{__WARN__} = sub {  };
-			*MT::Community::CMS::most_popular_entries_widget = \&most_popular_entries_widget;	
-		}
-	}
-	require MT::CMS::Dashboard;
-	my $mt_generate_dashboard_stats = \&MT::CMS::Dashboard::generate_dashboard_stats;
-	{
-		local $SIG{__WARN__} = sub {  };
-		*MT::CMS::Dashboard::generate_dashboard_stats = \&generate_dashboard_stats;	
-	}
+    my $plugin = shift;
+    my ($app) = @_;
+    eval { require MT::Community::CMS; };
+    if (!$@) {
+        my $mt_most_popular_entries_widget = \&MT::Community::CMS::most_popular_entries_widget;
+        {
+            local $SIG{__WARN__} = sub {  };
+            *MT::Community::CMS::most_popular_entries_widget = \&most_popular_entries_widget;
+        }
+    }
+    require MT::CMS::Dashboard;
+    my $mt_generate_dashboard_stats = \&MT::CMS::Dashboard::generate_dashboard_stats;
+    {
+        local $SIG{__WARN__} = sub {  };
+        *MT::CMS::Dashboard::generate_dashboard_stats = \&generate_dashboard_stats;
+    }
 }
 
 sub generate_dashboard_stats {
-	return 0;
+    return 0;
 }
 
 sub most_popular_entries_widget {
-	1;
+    1;
 }
 
 sub source_dashboard {
-	my ($cb, $app, $template) = @_;
-	my $old = q{<mt:setvarblock name="error"><__trans phrase="Movable Type was unable to locate your 'mt-static' directory. Please configure the 'StaticFilePath' configuration setting in your mt-config.cgi file, and create a writable 'support' directory underneath your 'mt-static' directory."></mt:setvarblock>};
-	$old = quotemeta($old);
-	$$template =~ s/$old//;
-	$old = q{<mtapp:statusmsg
+    my ($cb, $app, $template) = @_;
+    my $old = q{<mt:setvarblock name="error"><__trans phrase="Movable Type was unable to locate your 'mt-static' directory. Please configure the 'StaticFilePath' configuration setting in your mt-config.cgi file, and create a writable 'support' directory underneath your 'mt-static' directory."></mt:setvarblock>};
+    $old = quotemeta($old);
+    $$template =~ s/$old//;
+    $old = q{<mtapp:statusmsg
             id="generic-error"
             class="error">
             <mt:var name="error">
         </mtapp:statusmsg>};
     $old = quotemeta($old);
-	$$template =~ s/$old//;
+    $$template =~ s/$old//;
 }
 
 1;
